198f8ef06944ce810add13f76ed7964803fbdcd9
shashwat25dev <sd4610579@gmail.com>
2026-02-16 20:37:42 +0530

    Fix Redis auth for Railway

--- caching/redis.py
+++ caching/redis.py
@@ -2,7 +2,8 @@ import redis
 import os
 
 redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST", "localhost"),
+    host=os.getenv("REDIS_HOST"),
     port=int(os.getenv("REDIS_PORT", 6379)),
+    password=os.getenv("REDIS_PASSWORD"),   # requirement for railway
     decode_responses=True
 )
